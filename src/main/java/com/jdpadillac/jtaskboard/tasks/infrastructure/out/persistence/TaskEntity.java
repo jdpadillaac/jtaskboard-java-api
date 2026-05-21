26831bd6d5ae318e1d8e14ef21b60c7651b5642d
Jonatan Padilla <igjdpadillac@tutanota.com>
2026-05-21 14:51:55 -0500

    refactor TaskEntity to use Lombok for getter methods and clean up code

--- src/main/java/com/jdpadillac/jtaskboard/tasks/infrastructure/out/persistence/TaskEntity.java
+++ src/main/java/com/jdpadillac/jtaskboard/tasks/infrastructure/out/persistence/TaskEntity.java
@@ -9,9 +9,12 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Getter;
+
 import java.time.Instant;
 import java.util.UUID;
 
+@Getter
 @Entity
 @Table(name = "tasks")
 public class TaskEntity {
@@ -36,48 +39,24 @@ public class TaskEntity {
     @Column(name = "created_at", nullable = false)
     private Instant createdAt;
 
-    public UUID getId() {
-        return id;
-    }
-
     public void setId(UUID id) {
         this.id = id;
-    }
-
-    public String getTaskKey() {
-        return taskKey;
     }
 
     public void setTaskKey(String taskKey) {
         this.taskKey = taskKey;
     }
 
-    public String getTitle() {
-        return title;
-    }
-
     public void setTitle(String title) {
         this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
     }
 
     public void setDescription(String description) {
         this.description = description;
     }
 
-    public TaskStatus getStatus() {
-        return status;
-    }
-
     public void setStatus(TaskStatus status) {
         this.status = status;
-    }
-
-    public Instant getCreatedAt() {
-        return createdAt;
     }
 
     public void setCreatedAt(Instant createdAt) {
