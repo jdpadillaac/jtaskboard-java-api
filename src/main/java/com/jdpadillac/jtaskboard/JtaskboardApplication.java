11cdbebcb0e17775170ccd99087ec4061dcb978c
Jonatan Padilla <igjdpadillac@tutanota.com>
2026-05-21 13:25:47 -0500

    first commit

---
+++ src/main/java/com/jdpadillac/jtaskboard/JtaskboardApplication.java
@@ -0,0 +1,13 @@
+package com.jdpadillac.jtaskboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JtaskboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JtaskboardApplication.class, args);
+	}
+
+}
